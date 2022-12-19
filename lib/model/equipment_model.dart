e4be648086ec59300ec1abf497b7db4d01ead8dc
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-12-19 21:11:56 +0300

    item description and price now appearing

--- lib/model/equipment_model.dart
+++ lib/model/equipment_model.dart
@@ -2,31 +2,39 @@ import 'dart:convert';
 
 class Equipment {
   int id;
-  String title;
-  String image;
   int category;
+  String title;
+  String description;
+  double price;
+  String image;
   Equipment({
     required this.id,
-    required this.title,
-    required this.image,
     required this.category,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.image,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'title': title,
-      'image': image,
       'category': category,
+      'title': title,
+      'description': description,
+      'price': price,
+      'image': image,
     };
   }
 
   factory Equipment.fromMap(Map<String, dynamic> map) {
     return Equipment(
       id: map['id']?.toInt() ?? 0,
-      title: map['title'] ?? '',
-      image: map['image'] ?? '',
       category: map['category']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      price: map['price']?.toDouble() ?? 0.0,
+      image: map['image'] ?? '',
     );
   }
 
@@ -35,39 +43,3 @@ class Equipment {
   factory Equipment.fromJson(String source) =>
       Equipment.fromMap(json.decode(source));
 }
-
-// class Equipment {
-//   int id;
-//   String title;
-//   // int category;
-//   String image;
-//   Equipment({
-//     required this.id,
-//     required this.title,
-//     // required this.category,
-//     required this.image,
-//   });
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'id': id,
-//       'title': title,
-//       // 'category': category,
-//       'image': image,
-//     };
-//   }
-
-//   factory Equipment.fromMap(Map<String, dynamic> map) {
-//     return Equipment(
-//       id: map['id']?.toInt() ?? 0,
-//       title: map['title'] ?? '',
-//       // category: map['category']?.toInt() ?? 0,
-//       image: map['image'] ?? '',
-//     );
-//   }
-
-//   String toJson() => json.encode(toMap());
-
-//   factory Equipment.fromJson(String source) =>
-//       Equipment.fromMap(json.decode(source));
-// }
--- lib/util/my_tile.dart
+++ lib/util/my_tile.dart
@@ -148,7 +148,7 @@ class _MyTileState extends State<MyTile> {
                       ),
                       SizedBox(height: 5),
                       Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                        widget.item.description,
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(
@@ -162,7 +162,7 @@ class _MyTileState extends State<MyTile> {
                       Row(
                         children: [
                           Text(
-                            "KD 3",
+                            "KD ${widget.item.price}",
                             style: TextStyle(
                               color: Colors.black,
                               fontSize: 14,
