e8137d82bd9ce49a0ace1bccffec5db90003fc50
Khaled ALBuloushi <k.albloushii@hotmail.com>
2022-12-24 16:54:33 +0300

    updated splash screen and added scaling animation

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(Duration(milliseconds: 7500), () {
+    Future.delayed(Duration(milliseconds: 2500), () {
       context.go('/login');
     });
   }
@@ -22,33 +22,37 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.yellow[700],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                'assets/3idda-logo-black.png',
-                width: 300,
+      backgroundColor: Colors.yellow[700],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/3idda-logo-black.png',
+              width: 300,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          // const Text(
+          //   '"Our slogan goes here..."',
+          //   style: TextStyle(
+          //       fontSize: 20,
+          //       fontStyle: FontStyle.italic,
+          //       color: Colors.black87),
+          // ),
+        ],
+      )
+          .animate(
+            delay: Duration(milliseconds: 500),
+          )
+          .fade(duration: Duration(milliseconds: 500))
+          .scale(
+              duration: Duration(
+                milliseconds: 1000,
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            const Text(
-              '"Our slogan goes here..."',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black87),
-            ),
-          ],
-        )
-            .animate(
-              delay: Duration(milliseconds: 1000),
-            )
-            .fade(duration: Duration(milliseconds: 3000))
-        // .scale(duration: Duration(milliseconds: 500)),
-        );
+              begin: Offset(40, 40)),
+    );
   }
 }
