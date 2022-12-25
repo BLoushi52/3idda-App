88977c067454d338e141f5ad2c0b744f3fa198e0
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-12-25 16:47:46 +0300

    done with location on home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:rental_app/constraints.dart';
 import 'package:rental_app/screen/pages/account.dart';
@@ -9,8 +12,6 @@ import 'package:rental_app/screen/pages/myorder.dart';
 import 'package:rental_app/widgets/my_drawer.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-import 'pages/myfavorite.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -19,14 +20,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // Future<void> _handleRefresh() async {
-  //   return await context.read<ItemProvider>().loadItems();
-  // }
+  Position? _position;
+  List<Placemark>? placemarks;
 
-  // TextEditingController? textEditingController = TextEditingController();
+  void _getCurrentLocation() async {
+    Position position = await _determinePosition();
+    placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    setState(() {
+      _position = position;
+
+      print(placemarks);
+    });
+  }
+
+  Future<Position> _determinePosition() async {
+    LocationPermission permission;
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location Permissions are denied');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
   final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  // int _navBarPageSelector = 0;
 
   int _selectedIndex = 0;
   void _navigateBottomBar(int index) {
@@ -47,94 +71,82 @@ class _HomeScreenState extends State<HomeScreen> {
     return ResponsiveBuilder(
         builder: (BuildContext context, SizingInformation sizingInformation) {
       return Scaffold(
+          floatingActionButton: FloatingActionButton(
+              onPressed: _getCurrentLocation,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add)),
           key: scaffoldKey,
           backgroundColor: myDefaultBackground,
           appBar: AppBar(
-              leadingWidth: 68,
-              toolbarHeight: 40,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 25),
-                  child: Container(
-                    height: 40,
+            leadingWidth: 68,
+            toolbarHeight: 40,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 25),
+                child: Container(
+                  height: 40,
+                  width: 45,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.yellow[700]),
+                  // color: Colors.yellow[700],
+                  child: CupertinoButton(
+                    padding: EdgeInsets.only(right: 0),
+                    child: Icon(
+                      Icons.notifications_outlined,
+                      color: Colors.black,
+                      size: 25,
+                    ), //* Icon(Icons.notifications_active_outlined)
+                    onPressed: () {
+                      // Navigator.push(context, route);
+                    },
+                  ),
+                ),
+              )
+            ],
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.pin_drop_outlined),
+                SizedBox(width: 5),
+                _position != null && placemarks != null
+                    ? AutoSizeText(
+                        '${placemarks?.first.street}',
+                        style: TextStyle(fontSize: 10),
+                        minFontSize: 12,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    : AutoSizeText('No Location Data'),
+              ],
+            ),
+            backgroundColor: Colors.grey[300],
+            foregroundColor: Colors.black,
+            elevation: 0,
+            leading: Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Container(
                     width: 45,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Colors.yellow[700]),
-                    // color: Colors.yellow[700],
                     child: CupertinoButton(
-                      padding: EdgeInsets.only(right: 0),
-                      child: Icon(
-                        Icons.notifications_outlined,
-                        color: Colors.black,
-                        size: 25,
-                      ), //* Icon(Icons.notifications_active_outlined)
-                      onPressed: () {
-                        // Navigator.push(context, route);
-                      },
-                    ),
-                  ),
-                )
-              ],
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Surra, Kuwait',
-                    style: TextStyle(
-                      fontSize: 16,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(width: 6),
-                  Icon(Icons.pin_drop_outlined),
-                ],
-              ),
-              backgroundColor: Colors.grey[300],
-              foregroundColor: Colors.black,
-              elevation: 0,
-              leading: Padding(
-                  padding: const EdgeInsets.only(left: 25),
-                  child: Container(
-                      width: 45,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.yellow[700]),
-                      child: CupertinoButton(
-                          padding: EdgeInsets.only(right: 0),
-                          child: Icon(
-                            Icons.menu,
-                            color: Colors.black,
-                          ),
-                          onPressed: () {
-                            if (scaffoldKey.currentState!.isDrawerOpen) {
-                              scaffoldKey.currentState!.closeDrawer();
-                              //close drawer, if drawer is open
-                            } else {
-                              scaffoldKey.currentState!.openDrawer();
-                              //open drawer, if drawer is closed
-                            }
-                          })))),
-          drawer:
-              MyDrawer(), //*    <----- CHANGE DRAWER WIDGET IF GUEST/SIGNED IN
-          // bottomNavigationBar: _bottomNav(sizingInformation), //* <-- OLD BOTTOM NAV
-          // bottomNavigationBar: BottomNavigationBar(   //*  <-- NAV BAR 2
-          //   selectedItemColor: Colors.black,
-          //   currentIndex: _selectedIndex,
-          //   onTap: _navigateBottomBar,
-          //   type: BottomNavigationBarType.fixed,
-          //   backgroundColor: Colors.grey[300],
-          //   elevation: 0,
-          //   items: [
-          //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          //     BottomNavigationBarItem(
-          //         icon: Icon(Icons.list_alt), label: 'My Rentals'),
-          //     BottomNavigationBarItem(
-          //         icon: Icon(Icons.handyman_outlined), label: 'My Equipment'),
-          //     BottomNavigationBarItem(
-          //         icon: Icon(Icons.person_outlined), label: 'Account'),
-          //   ],
-          // ),
+                        padding: EdgeInsets.only(right: 0),
+                        child: Icon(
+                          Icons.menu,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          if (scaffoldKey.currentState!.isDrawerOpen) {
+                            scaffoldKey.currentState!.closeDrawer();
+                            //close drawer, if drawer is open
+                          } else {
+                            scaffoldKey.currentState!.openDrawer();
+                            //open drawer, if drawer is closed
+                          }
+                        }))),
+          ),
+          drawer: MyDrawer(),
           bottomNavigationBar: Container(
             color: Colors.grey[300],
             child: Padding(
@@ -177,107 +189,4 @@ class _HomeScreenState extends State<HomeScreen> {
           body: _pages[_selectedIndex]);
     });
   }
-
-  // Widget _bottomNav(SizingInformation sizingInformation) {
-  //   return Container(
-  //     height: sizingInformation.screenSize.height * 0.09,
-  //     decoration: BoxDecoration(color: Colors.grey[300]),
-  //     child: Row(
-  //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //       children: <Widget>[
-  //         _navBarItem(
-  //           title: 'Home',
-  //           image: 'assets/home.png',
-  //           onPressed: () {
-  //             setState(() {
-  //               _navBarPageSelector = 0;
-  //             });
-  //           },
-  //           selectedItemColor: _navBarPageSelector == 0
-  //               ? Colors.yellow[700]
-  //               : Colors.grey[300],
-  //           selectedItemIconTextColor:
-  //               _navBarPageSelector == 0 ? Colors.black : Colors.black,
-  //         ),
-  //         _navBarItem(
-  //           title: 'My Tools',
-  //           image: 'assets/tools-icon.png',
-  //           onPressed: () {
-  //             setState(() {
-  //               _navBarPageSelector = 1;
-  //             });
-  //           },
-  //           selectedItemColor: _navBarPageSelector == 1
-  //               ? Colors.yellow[700]
-  //               : Colors.grey[300],
-  //           selectedItemIconTextColor:
-  //               _navBarPageSelector == 1 ? Colors.black : Colors.black,
-  //         ),
-  //         _navBarItem(
-  //           title: 'Chat',
-  //           image: 'assets/chat.png',
-  //           onPressed: () {
-  //             setState(() {
-  //               _navBarPageSelector = 2;
-  //             });
-  //           },
-  //           selectedItemColor: _navBarPageSelector == 2
-  //               ? Colors.yellow[700]
-  //               : Colors.grey[300],
-  //           selectedItemIconTextColor:
-  //               _navBarPageSelector == 2 ? Colors.black : Colors.black87,
-  //         ),
-  //         _navBarItem(
-  //           title: 'Account',
-  //           image: 'assets/user-icon.png',
-  //           onPressed: () {
-  //             setState(() {
-  //               _navBarPageSelector = 3;
-  //             });
-  //           },
-  //           selectedItemColor: _navBarPageSelector == 3
-  //               ? Colors.yellow[700]
-  //               : Colors.grey[300],
-  //           selectedItemIconTextColor:
-  //               _navBarPageSelector == 3 ? Colors.black : Colors.grey[700],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
-  // Widget _navBarItem(
-  //     {required String title,
-  //     required String image,
-  //     VoidCallback? onPressed,
-  //     Color? selectedItemColor,
-  //     Color? selectedItemIconTextColor}) {
-  //   return InkWell(
-  //     onTap: onPressed,
-  //     child: Container(
-  //       padding: EdgeInsets.symmetric(horizontal: 10),
-  //       width: 100,
-  //       decoration: BoxDecoration(
-  //           color: selectedItemColor,
-  //           borderRadius: BorderRadius.only(
-  //               topLeft: Radius.circular(10), topRight: Radius.circular(10))),
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: <Widget>[
-  //           Container(
-  //             height: 25,
-  //             child: Image.asset(
-  //               image,
-  //               color: selectedItemIconTextColor,
-  //             ),
-  //           ),
-  //           SizedBox(height: 3),
-  //           Text(
-  //             title,
-  //             style: TextStyle(fontSize: 14, color: selectedItemIconTextColor),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
 }
