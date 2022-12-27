080ab5faa97314b3d5f00800147363eacb3ac481
Khaled ALBuloushi <k.albloushii@hotmail.com>
2022-12-27 22:00:07 +0300

    returned client to localhost instead of deployed server

--- lib/client.dart
+++ lib/client.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 
 class Client {
-  static final Dio dio =
-      Dio(BaseOptions(baseUrl: "http://bloushi52.pythonanywhere.com"));
+  static final Dio dio = Dio(BaseOptions(baseUrl: "http://localhost:8000"));
 }
