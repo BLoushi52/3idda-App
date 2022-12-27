7663fbf33141752591e2c968e4c8f7d214c45ad4
Khaled ALBuloushi <k.albloushii@hotmail.com>
2022-12-27 19:59:10 +0300

    updated styling of home page

--- lib/client.dart
+++ lib/client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 
 class Client {
-  static final Dio dio = Dio(BaseOptions(baseUrl: "http://localhost:8000"));
+  static final Dio dio =
+      Dio(BaseOptions(baseUrl: "http://bloushi52.pythonanywhere.com"));
 }
